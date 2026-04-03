1813ed19240fd72eeda02be676f043712ee973cc
Aleena C B <184249015+aleenacb@users.noreply.github.com>
2026-04-03 22:58:11 +0530

    Add subarray_sum function to count subarray sums
    Implement function to count subarrays summing to k.

---
+++ Sub_ArraySum.py
@@ -0,0 +1,14 @@
+def subarray_sum(nums, k):
+    count = 0
+    prefix = 0
+    d = {0:1}
+
+    for num in nums:
+        prefix += num
+        if prefix - k in d:
+            count += d[prefix - k]
+        d[prefix] = d.get(prefix, 0) + 1
+
+    return count
+
+print(subarray_sum([1,1,1], 2))
