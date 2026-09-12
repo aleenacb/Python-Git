9136ddedffcbf96faa00344c0b67bab5b15aa45c
TechWithAleenaCB <184249015+aleenacb@users.noreply.github.com>
2026-09-12 21:44:38 +0530

    Add files via upload

---
+++ Python Project/PandasDF.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df = pd.DataFrame()
+print(df)
+lst = ['Geeks','For','Geeks']
+df = pd.DataFrame(lst)
+print(df)
---
+++ Python Project/PandasSeries.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+data = np.array(['g','e','e'])
+s = pd.Series(data)
+print("pandas series")
+print(s)
